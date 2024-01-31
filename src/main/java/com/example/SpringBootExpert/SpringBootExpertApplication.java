e3ef97ec40d7e4577c45e0b987483483622f0112
jvmaiaa <joaovictormaia482@gmail.com>
2024-01-31 17:28:41 -0300

    Criando as entidades

---
+++ src/main/java/com/example/SpringBootExpert/SpringBootExpertApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootExpert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExpertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExpertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBootExpert/SpringBootExpertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootExpert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExpertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
