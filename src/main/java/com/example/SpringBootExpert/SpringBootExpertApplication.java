2f817b053437c061d044fb16120a679d0a55db35
jvmaiaa <joaovictormaia482@gmail.com>
2024-02-01 16:42:04 -0300

    Query methods. Para consultas mais simples.

--- src/main/java/com/example/SpringBootExpert/SpringBootExpertApplication.java
+++ src/main/java/com/example/SpringBootExpert/SpringBootExpertApplication.java
@@ -16,35 +16,12 @@ public class SpringBootExpertApplication {
 	@Bean
 	public CommandLineRunner init(@Autowired Clientes clientes) {
 		return args -> {
-			clientes.save(new Cliente("João"));
+			System.out.println("Salvando clientes");
+			clientes.save(new Cliente("José"));
 			clientes.save(new Cliente("Outro cliente"));
 
-			List<Cliente> todosClientes = clientes.findAll();
-			todosClientes.forEach(System.out::println);
-
-			System.out.println("Atualizando clientes");
-			todosClientes.forEach(c -> {
-				c.setNome(c.getNome() + " atualizado.");
-				clientes.save(c);
-			});
-
-			todosClientes = clientes.findAll();
-			todosClientes.forEach(System.out::println);
-
-			System.out.println("Buscando clientes");
-			clientes.findByNomeLike("Cli").forEach(System.out::println);
-
-			System.out.println("deletando clientes");
-			clientes.findAll().forEach(c -> {
-				clientes.delete(c);
-			});
-
-			todosClientes = clientes.findAll();
-			if(todosClientes.isEmpty()){
-				System.out.println("Nenhum cliente encontrado.");
-			}else{
-				todosClientes.forEach(System.out::println);
-			}
+			boolean existe = clientes.existsByNome("João");
+			System.out.println("Existe um cliente com o nome João? " + existe);
 		};
 	}
 
--- src/main/java/com/example/SpringBootExpert/domain/repositorio/Clientes.java
+++ src/main/java/com/example/SpringBootExpert/domain/repositorio/Clientes.java
@@ -11,4 +11,6 @@ import java.util.List;
 public interface Clientes extends JpaRepository<Cliente, Integer> {
 
     List<Cliente> findByNomeLike(String nome);
+
+    boolean existsByNome(String nome);
 }
