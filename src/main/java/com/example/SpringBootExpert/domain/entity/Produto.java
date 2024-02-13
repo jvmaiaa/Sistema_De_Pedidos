1e670075e02037a7f18a4aaea224c22f8023038d
jvmaiaa <joaovictormaia482@gmail.com>
2024-02-13 10:49:25 -0300

    Implementa internacionalização

--- src/main/java/com/example/SpringBootExpert/domain/entity/Produto.java
+++ src/main/java/com/example/SpringBootExpert/domain/entity/Produto.java
@@ -20,11 +20,11 @@ public class Produto {
     private Integer id;
 
     @Column(name = "DESCRICAO")
-    @NotEmpty(message = "Campo Descrição é obrigatório.")
+    @NotEmpty(message = "{campo.decricao.obrigatorio}")
     private String descricao;
 
     @Column(name = "PRECO_UNITARIO")
-    @NotNull(message = "Campo Preço é obrigatório.")
+    @NotNull(message = "{campo.preco.obrigatorio}")
     private BigDecimal preco;
 
 }
